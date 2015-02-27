93ed550965cc8025c90ad9d20a4525a32b33dd0d
Nathan Townsend <ntownsend2@mt.gov>
2015-02-27 16:03:01 -0700

    use ng-model for 2-way data binding

--- MvcDemo/MvcDemo/app/EmployeeForm/efController.js
+++ MvcDemo/MvcDemo/app/EmployeeForm/efController.js
@@ -3,6 +3,17 @@
 // a scope that uses a reference to service. Sets employee to services employee field
 angularFormsApp.controller('efController',
     function efController($scope, efService) {
-        // sets the scope variable for employee to service employee
+
+        // this is the model
         $scope.employee = efService.employee;
+
+        $scope.departments = [
+                "Engineering",
+                "Marketing",
+                "Finance",
+                "Administration"
+        ];
+
+        // is accessed with ng-model in form for 2-way binding
+        // do not use ng-bind or {{ }} for 2 way binding, they are for 1-way binding only
     });
